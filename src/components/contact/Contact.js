c923ca6506b5f08608f7fa6ada78f5b37973c3e8
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2023-12-30 02:36:34 -0500

    emailjs added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,27 +1,74 @@
 import './Contact.css';
 
-
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const serviceId = "service_pbtbshk";
+    const templateId = "template_b4b4ly7";
+    const publicKey = "JxcTAhgwPAFVho8mf";
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let templateParams = {
+            from_name: name,
+            from_phone: phone,
+            from_email: email,
+            message: message
+          };
+          emailjs.send(serviceId, templateId, templateParams, publicKey) //use your Service ID and Template ID
+            .then(function (response) {
+              console.log('SUCCESS!', response.status, response.text);
+              setName('');
+              setEmail('');
+              setMessage('');
+              setPhone('');
+            }, function (error) {
+              console.log('FAILED...', error);
+            });
+    }
     return (
         <section className="contact">
             <article className="contact-form">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
-                        placeholder="Email Address"
-                        type="email"
+                        placeholder="Your Name"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         required
                     />
-                    <input 
-                        type="text"
-                        placeholder="Subject"
+                    <input
+                        placeholder="Your Email Address"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <input
+                        placeholder="Your Phone Number (Pattern: 123-456-7890)"
+                        type="tel"
+                        name="phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
                         required
                     />
                     <textarea
                         placeholder="Write your message here."
                         cols="20"
                         rows="10"
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
                         required
                     />
                     <button type="submit">Send Message</button>
