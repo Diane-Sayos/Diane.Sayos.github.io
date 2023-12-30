54b7fe9440e5b10a41510fbfe07cf7f26e17b15a
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2023-12-30 01:41:48 -0500

    change contact file

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,24 +1,11 @@
 import './Contact.css';
 
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import CallIcon from '@mui/icons-material/Call';
-import EmailIcon from '@mui/icons-material/Email';
-
 
 
 
 const Contact = () => {
     return (
         <section className="contact">
-            <Stack direction="row" spacing={2}>
-            <Button variant="outlined" startIcon={<CallIcon />}>Call</Button>
-            <Button variant="outlined" startIcon={<EmailIcon />}>Email</Button>
-            </Stack>
-            <div className="buttons">
-                <button className="contact-buttons">Mail</button>
-                
-            </div>
             <article className="contact-form">
                 <form>
                     <input
