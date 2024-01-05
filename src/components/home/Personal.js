497cf2d7e73ae6bef587f71c395fc82ddac13281
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2024-01-05 14:29:43 -0500

    remove buttons from personal

--- src/components/home/Personal.js
+++ src/components/home/Personal.js
@@ -1,28 +1,11 @@
 import './Home.css';
 
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import DownloadIcon from '@mui/icons-material/Download';
-
-import resume from '../../images/DianeSayos-Briones.pdf';
-import businessCard from '../../images/businesscard.png';
-
 
 
 const Personal = () => {
     return (
         <section>
             <article className="personal-info">
-                <div className="downloads">
-                    <Stack direction="row" spacing={2}>
-                        <Button variant="outlined" startIcon={<DownloadIcon />}>
-                            <a href={resume} download className="download-button">Resume</a>
-                        </Button>
-                        <Button variant="outlined" startIcon={<DownloadIcon />}>
-                        <a href={businessCard} download className="download-button">Business Card</a>
-                        </Button>
-                    </Stack>
-                </div>
                 <h3>PERSONAL INFORMATION</h3>
                 <h4>Email:</h4>
                 <p>dianesayosbriones@gmail.com</p>
