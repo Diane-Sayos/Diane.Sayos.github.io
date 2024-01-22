8c0600088458ff942085d6fe3f253b3dc14e4e91
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2024-01-22 16:43:10 -0500

    headers adjusted

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import avatar from '../../images/personal/avatar.JPG';
+import Avatar from '@mui/material/Avatar';
 
 import './Header.css';
 
@@ -10,18 +11,16 @@ const Header = () => {
 
     return (
         <header className="navigation">
-            <div className="header">
-                <div className="img-container">
-                <img src={avatar} alt="avatar" />
-                </div>
-                <h1>Diane Sayos-Briones</h1>
-                <p>Software Engineer</p>
-            </div>
             <nav className="navbar">
-                <NavLink to="/" onClick={() => setActiveNav('/')}className={activeNav === '/' ? "navbar-link-selected" : "navbar-link"}>HOME</NavLink>
-                <NavLink to="/certifications" onClick={() => setActiveNav('/certifications')}className={activeNav === '/certifications' ? "navbar-link-selected" : "navbar-link"}>CERTIFICATIONS</NavLink>
-                <NavLink to="/projects" onClick={() => setActiveNav('/projects')}className={activeNav === '/projects' ? "navbar-link-selected" : "navbar-link"}>PROJECTS</NavLink>
-                <NavLink to="/contact" onClick={() => setActiveNav('/contact')}className={activeNav === '/contact' ? "navbar-link-selected" : "navbar-link"}>CONTACT</NavLink>
+                <div className="navbar-avatar-container">
+                    <Avatar sx={{}} alt="Diane Sayos-Briones" src={avatar} />
+                </div>
+                <div className="navbar-container">
+                    <NavLink to="/" onClick={() => setActiveNav('/')}className={activeNav === '/' ? "navbar-link-selected" : "navbar-link"}>HOME</NavLink>
+                    <NavLink to="/certifications" onClick={() => setActiveNav('/certifications')}className={activeNav === '/certifications' ? "navbar-link-selected" : "navbar-link"}>CERTIFICATIONS</NavLink>
+                    <NavLink to="/projects" onClick={() => setActiveNav('/projects')}className={activeNav === '/projects' ? "navbar-link-selected" : "navbar-link"}>PROJECTS</NavLink>
+                    <NavLink to="/contact" onClick={() => setActiveNav('/contact')}className={activeNav === '/contact' ? "navbar-link-selected" : "navbar-link"}>CONTACT</NavLink>
+                </div>
             </nav>
         </header>
     )
