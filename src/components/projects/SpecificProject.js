e43ff41d440acfbbfea2a2b1bce4cae7362da5ec
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2024-01-22 02:36:12 -0500

    adjusting project format

--- src/components/projects/SpecificProject.js
+++ src/components/projects/SpecificProject.js
@@ -9,19 +9,19 @@ import { CardActionArea } from '@mui/material';
 
 const SpecificProject = ({ project }) => {
     return (
-        <Card raised sx={{width:300, height:370, p:"0.5rem", m:"0.5rem auto", borderRadius:"1em"}} >
+        <Card raised sx={{width:300, height:370, p:"0.5rem", m:"1rem auto", borderRadius:"1em"}} >
             <ImageCarousel images={project.images}/>
-            <CardActionArea>
-            <CardContent sx={{margin:"10px", p:"10px"}}>
-                <Typography variant="h6" component="div" textAlign="center" color="#7f5539">
-                    {project.name}
-                </Typography>
-                <Typography variant="body2" color="#7f5539">
-                    {project.description}
-                </Typography>
-            </CardContent>
-        </CardActionArea>
-     </Card>
+            <CardActionArea onClick={() => window.open(`${project.link}`, "_blank")}>
+                <CardContent sx={{margin:"10px", p:"10px"}}>
+                    <Typography variant="h6" component="div" textAlign="center" color="#7f5539">
+                        {project.name}
+                    </Typography>
+                    <Typography variant="body2" color="#7f5539">
+                        {project.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     )
 };
 
