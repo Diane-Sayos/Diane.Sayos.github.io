de8a37b3fe078e27ec849110168f51d82f75852b
divineheart <100250310+Diane-Sayos@users.noreply.github.com>
2024-01-22 03:26:05 -0500

    footer adjusted to material ui

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,3 @@
-import './Footer.css';
-
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -9,6 +7,7 @@ import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 import MailIcon from '@mui/icons-material/Mail';
 import CallIcon from '@mui/icons-material/Call';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
+import Paper from '@mui/material/Paper';
 
 import resume from '../../images/personal/DianeSayos-Briones.pdf';
 import businessCard from '../../images/personal/businesscard.png';
@@ -17,35 +16,17 @@ import businessCard from '../../images/personal/businesscard.png';
 const Footer = () => {
     
     return (
-        <section className="footer">
-            <div className="container" onClick={() => window.open("https://www.linkedin.com/in/diane-sayos-briones/", "_blank")}>
-                <LinkedInIcon />
-            </div>
-            <div className="container" onClick={() => window.open("https://github.com/Diane-Sayos", "_blank")}>
-                <GitHubIcon />
-            </div>
-            <div className="container" onClick={() => window.open("https://www.facebook.com/dianenazares", "_blank")}>
-                <FacebookIcon />
-            </div>
-            <div className="container" onClick={() => window.open("https://www.instagram.com/_divineheart_/", "_blank")}>
-                <InstagramIcon />
-            </div>
-            <div className="container" onClick={() => window.open("https://calendly.com/dianesayosbriones", "_blank")}>
-                <CalendarMonthIcon />
-            </div>
-            <div className="container">
-                <a href="mailto:dianesayosbriones@gmail.com" target="_blank" rel="noreferrer"><MailIcon /></a>
-            </div>
-            <div className="container">
-                <a href={resume} download className="download-button"><StickyNote2Icon /></a>
-            </div>
-            <div className="container">
-                <a href={businessCard} download className="download-button"><CreditCardIcon /></a>
-            </div>
-            <div className="container">
-                <a href="tel:+13473487287" target="_blank" rel="noreferrer"><CallIcon /></a>
-            </div>
-        </section>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, p:0, display:"flex", justifyContent:"space-evenly" }} elevation={4}>
+            <LinkedInIcon sx={{m:"0.5rem"}} onClick={() => window.open("https://www.linkedin.com/in/diane-sayos-briones/", "_blank")}/>
+            <GitHubIcon sx={{m:"0.5rem"}} onClick={() => window.open("https://github.com/Diane-Sayos", "_blank")}/>
+            <FacebookIcon sx={{m:"0.5rem"}} onClick={() => window.open("https://www.facebook.com/dianenazares", "_blank")}/>
+            <InstagramIcon sx={{m:"0.5rem"}} onClick={() => window.open("https://www.instagram.com/_divineheart_/", "_blank")}/>
+            <CalendarMonthIcon sx={{m:"0.5rem"}} onClick={() => window.open("https://calendly.com/dianesayosbriones", "_blank")}/>
+            <a href="mailto:dianesayosbriones@gmail.com" target="_blank" rel="noreferrer"><MailIcon sx={{m:"0.5rem"}}/></a>
+            <a href={resume} download className="download-button"><StickyNote2Icon sx={{m:"0.5rem"}}/></a>
+            <a href={businessCard} download className="download-button"><CreditCardIcon sx={{m:"0.5rem"}}/></a>
+            <a href="tel:+13473487287" target="_blank" rel="noreferrer"><CallIcon sx={{m:"0.5rem"}}/></a>
+        </Paper>
     )
 };
 
